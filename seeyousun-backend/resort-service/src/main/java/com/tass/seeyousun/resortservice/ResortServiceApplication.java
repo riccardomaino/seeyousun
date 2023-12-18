5ccf2477738de75b79be7acf5f0347934be5396f
Riccardo Maino <riccardo.maino8@gmail.com>
2023-12-18 13:22:07 +0100

    Minor changes to resort-service

--- seeyousun-backend/resort-service/src/main/java/com/tass/seeyousun/resortservice/ResortServiceApplication.java
+++ seeyousun-backend/resort-service/src/main/java/com/tass/seeyousun/resortservice/ResortServiceApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ResortServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ResortServiceApplication.class, args);
     }
