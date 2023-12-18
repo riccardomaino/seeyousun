d7c5072e22128d7f45768c230ed7dc7ef3814d14
Riccardo Maino <riccardo.maino8@gmail.com>
2023-12-18 13:07:16 +0100

    Added resort-service module

---
+++ seeyousun-backend/resort-service/src/main/java/com/tass/seeyousun/resortservice/ResortServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tass.seeyousun.resortservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResortServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResortServiceApplication.class, args);
+    }
+
+}
---
+++ seeyousun-backend/resort-service/src/test/java/com/tass/seeyousun/resortservice/ResortServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tass.seeyousun.resortservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResortServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
