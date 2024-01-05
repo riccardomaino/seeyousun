7025e8dded305cf182ce678b320c63600ca07ca2
rovaretto <emanuele.rovaretto@edu.unito.it>
2024-01-05 18:37:04 +0100

    main

---
+++ seeyousun-backend/resort-reservation-service/src/main/java/com/taass/seeyousun/resortreservationservice/ResortReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.taass.seeyousun.resortreservationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResortReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResortReservationServiceApplication.class, args);
+	}
+
+}
