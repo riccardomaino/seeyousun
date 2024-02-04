bb750f6e42a3391127cd0bec2af01f6ba988dd9c
Riccardo Maino <riccardo.maino8@gmail.com>
2024-02-04 06:01:46 +0100

    Fix API Gateway and env variables refactoring

--- seeyousun-backend/resort-service/src/main/java/com/taass/seeyousun/resortservice/ResortServiceApplication.java
+++ seeyousun-backend/resort-service/src/main/java/com/taass/seeyousun/resortservice/ResortServiceApplication.java
@@ -2,8 +2,10 @@ package com.taass.seeyousun.resortservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ResortServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ResortServiceApplication.class, args);
--- seeyousun-backend/review-service/src/main/java/com/taass/seeyousun/reviewservice/ReviewServiceApplication.java
+++ seeyousun-backend/review-service/src/main/java/com/taass/seeyousun/reviewservice/ReviewServiceApplication.java
@@ -2,10 +2,11 @@ package com.taass.seeyousun.reviewservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ReviewServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewServiceApplication.class, args);
 	}
