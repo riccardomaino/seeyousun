ad8a1a2578257467ba7aa26464970d35eae94915
rovaretto <emanuele.rovaretto@edu.unito.it>
2024-02-06 17:18:44 +0100

    review db

---
+++ seeyousun-backend/review-service/src/main/resources/sql/reviews.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.reviews (id, date, description, rating, resort_id, title) VALUES
+                                                                                 (1, '2024-02-05', 'Fantasioso e incantevole', 4, 1, 'Un sogno nel verde'),
+                                                                                 (2, '2024-02-05', 'Buon cibo, ma servizio lento.', 3, 1, 'Gusto con calma'),
+                                                                                 (3, '2024-02-05', 'Vista spettacolare!', 5, 1, 'Panorama mozzafiato'),
+                                                                                 (4, '2024-02-05', 'Stanze pulite e accoglienti.', 4, 1, 'Comfort a misura di viaggiatore'),
+                                                                                 (5, '2024-02-05', 'Personale cordiale e disponibile.', 5, 1, 'Ospitalità premurosa'),
+                                                                                 (6, '2024-02-05', 'Camere rumorose di notte.', 2, 1, 'Silenzio disturbato'),
+                                                                                 (7, '2024-02-05', 'Cibo mediocre, ma posizione fantastica.', 3, 2, 'Tra gusto e vista');
