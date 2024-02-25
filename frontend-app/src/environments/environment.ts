1784f29bff77c569cb6af9fb73d55ca32bfe6fc4
Riccardo Maino <riccardo.maino8@gmail.com>
2024-02-25 23:03:27 +0100

    Fix Firebase

--- frontend-app/src/environments/environment.ts
+++ frontend-app/src/environments/environment.ts
@@ -1,5 +1,12 @@
 export const environment = {
     production: true,
     backendUrl: "http://localhost:8080",
-    firebaseConfig: {},
+    firebaseConfig: {
+        apiKey: "AIzaSyAFr1cyXBFwlZ2qVrbX_tZzqrmkEZDBhJg",
+        authDomain: "seeyousun-896e1.firebaseapp.com",
+        projectId: "seeyousun-896e1",
+        storageBucket: "seeyousun-896e1.appspot.com",
+        messagingSenderId: "916483774888",
+        appId: "1:916483774888:web:1dcec251213f421060259d"
+    },
 };
